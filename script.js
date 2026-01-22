562534b52fe94db5f6397b23f32a839bffb7d39e
theclothsurban <theclothsurban@gmail.com>
2026-01-22 16:43:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+  alert("Product added to cart");
+}
+
+function updateCartCount() {
+  document.getElementById("cartCount").innerText = cart.length;
+}
+
+updateCartCount();
 
